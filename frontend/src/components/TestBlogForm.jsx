5cc2466be0b6a662f3031f21c929329494f0206b
Jay Pawar <140436138+jaypawar2004@users.noreply.github.com>
2025-01-20 23:00:31 +0530

    Update TestBlogForm.jsx

--- frontend/src/components/TestBlogForm.jsx
+++ frontend/src/components/TestBlogForm.jsx
@@ -9,6 +9,7 @@ const TestBlogForm = () => {
 
   return (
     <div className="container mx-auto p-4">
+{/*       test blog */}
       <h1 className="text-2xl font-bold mb-4">Test Blog Form</h1>
       <BlogForm onAddBlog={handleAddBlog} />
     </div>
